d99edadbb57691e8f6fe994ed39972c3c6e08e5e
cotideff <a.constanzadefranchi@icea.edu.ar>
2024-10-08 19:03:18 +0000

    5

--- hola.py
+++ hola.py
@@ -1,1 +1,15 @@
-
+Lista = ('Constanza', 'Defranchi', 'Denisse',)
+Nombre = Lista[1]
+Apellido = Lista[2]
+String = ('Hola')
+Numeros = (233 + 3) - 2 - 5
+Numeros = 293
+Booleanos = (True)
+Booleanos = (False, True)
+if Booleanos[0]: 
+    print (23)
+    IT = 'pennywise'
+'''
+alt+9+1
+alt+9+3
+'''
